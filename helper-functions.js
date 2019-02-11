0e674601068e379bc831ebbdf16ee6460809b8b9
Jamie Forth <j.forth@gold.ac.uk>
2019-02-11 15:29:16 +0000

    sum(): ensure data are Number type

--- helper-functions.js
+++ helper-functions.js
@@ -1,6 +1,9 @@
 function sum(data) {
   var total = 0;
 
+  // Ensure that data contains numbers and not strings.
+  data = stringsToNumbers(data);
+
   for (let i = 0; i < data.length; i++) {
     total = total + data[i];
   }
