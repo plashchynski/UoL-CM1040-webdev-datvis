b6870feb7997f65c7aa0bfecb575ff35c7c205be
Dzmitry Plashchynski <plashchynski@gmail.com>
2021-06-28 12:55:20 +0300

    Add PieChart3D class

--- tech-diversity-race.js
+++ tech-diversity-race.js
@@ -46,7 +46,7 @@ function TechDiversityRace() {
   };
 
   // Create a new pie chart object.
-  this.pie = new PieChart(width / 2, height / 2, width * 0.4);
+  this.pie = new PieChart3D(width / 2, height / 2, width * 0.4);
 
   this.draw = function() {
     if (!this.loaded) {
