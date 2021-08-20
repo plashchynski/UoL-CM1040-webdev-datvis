7e526bcf7899e127c9778bc447b80c85d9c6594d
Dzmitry Plashchynski <plashchynski@gmail.com>
2021-08-20 23:43:54 +0300

    Move to new data source from WHO

--- covid-map.js
+++ covid-map.js
@@ -1,5 +1,4 @@
 function CovidMap() {
-
   // Name for the visualisation to appear in the menu bar.
   this.name = 'COVID-19 Map';
 
@@ -7,17 +6,57 @@ function CovidMap() {
   // characters.
   this.id = 'covid-map';
 
-  // Property to represent whether data has been loaded.
-  this.loaded = false;
-
   // Preload the data. This function is called automatically by the
   // gallery when a visualisation is added.
   this.preload = function() {
     // Calling loadTable() inside preload() guarantees to complete the operation before setup()
     // and draw() are called. https://p5js.org/reference/#/p5/loadTable
-    this.countryCodes = loadTable('./data/covid-19/country-codes.csv', 'csv', 'header');
-    this.dailyCases = loadTable('./data/covid-19/daily-cases.csv', 'csv', 'header');
+    this.covidGlobalData = loadTable('./data/covid-19/WHO-COVID-19-global-data.csv', 'csv', 'header');
     this.mapSvg = loadStrings('./data/covid-19/map.svg');
+  };
+
+  this.setup = function() {
+    // parse map svg sources to DOM document
+    var parser = new DOMParser();
+    this.map = parser.parseFromString(this.mapSvg.toString(), "image/svg+xml");
+
+    // reset map styles
+    this.map.querySelectorAll(".landxx,.subxx,.circlexx,.limitxx").forEach(function(el) {
+      el.style.stroke = "#ffffff";
+      el.style.strokeWidth = 0.5;
+      el.style.fillRule = "evenodd";
+      el.style.fill = "#c0c0c0";
+    });
+
+    // set date slider
+    this.days = Array.from(new Set(this.covidGlobalData.getColumn("Date_reported")));
+    this.dateSlider = createSlider(1, this.days.length, this.days.length, 1);
+    this.dateSlider.position(400, 20);
+
+    this.render_map();
+
+    var self = this;
+    this.dateSlider.input(function () {
+      self.render_map();
+    });
+  };
+
+  this.destroy = function() {
+    this.dateSlider.remove();
+  };
+
+  this.draw = function() {
+    image(this.mapImg, 50, 100, 826, 419);
+
+    // The Date constructor function accepts a set of ordered values that represent each part of a date:
+    // the year, the month (starting from 0), the day, the hour, the minutes, seconds and milliseconds
+    var date = new Date(this.date);
+    fill('black');
+    textAlign(LEFT, BASELINE);
+    textSize(12);
+    strokeWeight(0);
+    textStyle(BOLD);
+    text(date.toDateString(), 110, 50);
   };
 
   this.render_map = function() {
@@ -26,26 +65,27 @@ function CovidMap() {
     // generate an individual style for each country
     var countryColors = [];
 
-    this.date = this.dailyCases.columns[this.dateSlider.value()+2];
-    var maxValue = max(this.dailyCases.getColumn(this.date).map(Number));
-    var minValue = min(this.dailyCases.getColumn(this.date).map(Number));
+    this.date = this.days[this.dateSlider.value()-1];
+    this.dayData = this.covidGlobalData.findRows(this.date, "Date_reported");
 
-    this.dailyCases.rows.forEach(function(row) {
-      var Alpha3CountryCode = row.getString("id");
-      var countryCodeRow = self.countryCodes.findRow(Alpha3CountryCode, "Alpha-3 code");
-      if (!countryCodeRow)
+    newCases = this.dayData.map(row => row.getNum('New_cases'));
+    var maxValue = max(newCases);
+    var minValue = min(newCases);
+
+    this.dayData.forEach(function(row) {
+      var countryCode = row.getString('Country_code').toLowerCase();
+      if (countryCode == ' ')
         return;
-
-      var countryCode = countryCodeRow.getString("Alpha-2 code").toLowerCase();
 
       // set default colour for all countries
       self.map.querySelectorAll("#" + countryCode + ", #" + countryCode + " *").forEach(function(el) {
         el.style.fill = "#c0c0c0";
       });
 
-      var value = Number(row.getNum(self.date));
+      var value = Number(row.getNum('New_cases'));
       if (value <= 0)
         return;
+
       var c = color('red');
       c.setAlpha(map(value, minValue, maxValue, 50, 255));
       countryColors.push({
@@ -75,50 +115,4 @@ function CovidMap() {
     var blobURL = URL.createObjectURL(blob);
     this.mapImg = loadImage(blobURL);
   }
-
-  this.setup = function() {
-    // parse map svg sources to DOM document
-    var parser = new DOMParser();
-    this.map = parser.parseFromString(this.mapSvg.toString(), "image/svg+xml");
-
-    // reset map styles
-    this.map.querySelectorAll(".landxx,.subxx,.circlexx,.limitxx").forEach(function(el) {
-      el.style.stroke = "#ffffff";
-      el.style.strokeWidth = 0.5;
-      el.style.fillRule = "evenodd";
-      el.style.fill = "#c0c0c0";
-    });
-
-    // set date slider
-    var daysCount = this.dailyCases.columns.length - 3;
-    this.dateSlider = createSlider(0, daysCount, daysCount, 1);
-    this.dateSlider.position(400, 20);
-
-    this.render_map();
-
-    var self = this;
-    this.dateSlider.input(function () {
-      self.render_map();
-    });
-  };
-
-  this.destroy = function() {
-    this.dateSlider.remove();
-  };
-
-  this.draw = function() {
-    image(this.mapImg, 50, 100, 826, 419);
-
-    var parts = this.date.split('/');
-
-    // The Date constructor function accepts a set of ordered values that represent each part of a date:
-    // the year, the month (starting from 0), the day, the hour, the minutes, seconds and milliseconds
-    var date = new Date(("20" + parts[2]), parts[0]-1, parts[1]);
-    fill('black');
-    textAlign(LEFT, BASELINE);
-    textSize(12);
-    strokeWeight(0);
-    textStyle(BOLD);
-    text(date.toDateString(), 110, 50);
-  };
 }
