d59173354e0ae4281a22a74ac2116f3d26b866d6
Dzmitry Plashchynski <plashchynski@gmail.com>
2021-08-21 01:52:14 +0300

    Use SVG HTML rendering instead of img

--- covid-map.js
+++ covid-map.js
@@ -12,22 +12,13 @@ function CovidMap() {
     // Calling loadTable() inside preload() guarantees to complete the operation before setup()
     // and draw() are called. https://p5js.org/reference/#/p5/loadTable
     this.covidGlobalData = loadTable('./data/covid-19/WHO-COVID-19-global-data.csv', 'csv', 'header');
-    this.mapSvg = loadStrings('./data/covid-19/map.svg');
+    this.map = createDiv("<object type='image/svg+xml' data='/data/covid-19/BlankMap-World.svg' width='826' height='419'>");
+    this.map.style('visibility', 'hidden');
   };
 
   this.setup = function() {
     var self = this;
-    // parse map svg sources to DOM document
-    var parser = new DOMParser();
-    this.map = parser.parseFromString(this.mapSvg.toString(), "image/svg+xml");
-
-    // reset map styles
-    this.map.querySelectorAll(".landxx,.subxx,.circlexx,.limitxx").forEach(function(el) {
-      el.style.stroke = "#ffffff";
-      el.style.strokeWidth = 0.5;
-      el.style.fillRule = "evenodd";
-      el.style.fill = "#c0c0c0";
-    });
+    this.map.style('visibility', 'visible');
 
     // set date slider
     this.days = Array.from(new Set(this.covidGlobalData.getColumn("Date_reported")));
@@ -38,27 +29,30 @@ function CovidMap() {
     this.dataSetSelector.option('Daily cases');
     this.dataSetSelector.option('Daily deaths');
     this.dataSetSelector.option('Total deaths');
-    this.dataSetSelector.input(function () {
-      self.render_map();
-    });
 
     this.dateSlider = createSlider(1, this.days.length, this.days.length, 1);
     this.dateSlider.position(700, 540);
 
-    this.render_map();
+    this.map.position(350, 70);
+
+    this.dataSetSelector.input(function () {
+      self.render_map();
+    });
 
     this.dateSlider.input(function () {
       self.render_map();
     });
+
+    this.render_map();
   };
 
   this.destroy = function() {
     this.dateSlider.remove();
+    this.dataSetSelector.remove();
+    this.map.style('visibility', 'hidden');
   };
 
   this.draw = function() {
-    image(this.mapImg, 50, 100, 826, 419);
-
     // The Date constructor function accepts a set of ordered values that represent each part of a date:
     // the year, the month (starting from 0), the day, the hour, the minutes, seconds and milliseconds
     var date = new Date(this.date);
@@ -97,7 +91,7 @@ function CovidMap() {
         return;
 
       // set default colour for all countries
-      self.map.querySelectorAll("#" + countryCode + ", #" + countryCode + " *").forEach(function(el) {
+      self.map.elt.querySelector('object').contentDocument.querySelectorAll("#" + countryCode + ", #" + countryCode + " *").forEach(function(el) {
         el.style.fill = "#c0c0c0";
       });
 
@@ -108,23 +102,9 @@ function CovidMap() {
       var c = color('red');
       c.setAlpha(map(value, minValue, maxValue, 50, 255));
       // Set an individual style for each country
-      self.map.querySelectorAll("#" + countryCode + ", #" + countryCode + " *").forEach(function(el) {
+      self.map.elt.querySelector('object').contentDocument.querySelectorAll("#" + countryCode + ", #" + countryCode + " *").forEach(function(el) {
         el.style.fill = c.toString();
       });
     });
-
-    // loadImage raise an unknown error for some reason
-    // var svgText = self.map.querySelector('svg').textContent;
-    // var encodedData = "data:image/svg+xml;base64," + window.btoa(svgText);
-    // this.mapImg = loadImage(encodedData);
-
-    // render map image to blob
-    var svgElement = this.map.querySelector('svg');
-    var clonedSvgElement = svgElement.cloneNode(true);
-    var outerHTML = clonedSvgElement.outerHTML;
-    var blob = new Blob([outerHTML],{type:'image/svg+xml'});
-    var URL = window.URL || window.webkitURL || window;
-    var blobURL = URL.createObjectURL(blob);
-    this.mapImg = loadImage(blobURL);
   }
 }
